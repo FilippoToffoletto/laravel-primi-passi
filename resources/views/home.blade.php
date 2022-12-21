cc250db00493cfa846fc13b0f9fc4e1baf9a60a3
FilippoToffoletto <filippotoffoletto@hotmail.it>
2022-12-21 17:34:01 +0100

    [add]jumbo style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
 
     <title>Filippo's blog</title>
 </head>
 <body>
-    <h1>{{ $home }}</h1>
+    <header class="ft-container">
+        <img src="./img/logo.jpeg" alt="">
+        <h1>{{ $home }}</h1>
+    </header>
 
     <nav>
         <ul>
@@ -22,6 +28,34 @@
         </ul>
     </nav>
 
-    <img src="./logo.svg" alt="">
+    <main class="ft-container">
+        <div class="ft-container">
+
+            <section>
+
+                <!-- Background image -->
+                <div
+                  class="jumbo"
+                >
+                  <div
+                    class="ft-img"
+                    style="background-color: rgba(0, 0, 0, 0.6)"
+                  >
+                    <div class="ft-text">
+                      <div class="text-white">
+                        <h2 class="font-semibold text-4xl mb-4">Welcome</h2>
+                        <h4 class="font-semibold text-xl mb-6">In my blog</h4>
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <img src="" alt="">
+            </section>
+
+
+        </div>
+    </main>
+
 </body>
 </html>
